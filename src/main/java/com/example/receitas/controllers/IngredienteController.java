726a716329d58e703b8e90cff300dd7cd4a5507d
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-08-04 21:05:15 -0300

    atualizando

--- src/main/java/com/example/receitas/controllers/IngredienteController.java
+++ src/main/java/com/example/receitas/controllers/IngredienteController.java
@@ -1,9 +1,12 @@
 package com.example.receitas.controllers;
 
+import com.example.receitas.models.Ingrediente;
 import com.example.receitas.services.IngredienteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/ingredientes")
@@ -12,5 +15,9 @@ public class IngredienteController {
 
     private final IngredienteService ingredienteService;
 
-    
+    public List<Ingrediente> buscarTodosIngredientes() {
+        return ingredienteService.buscartodos();
+    }
+
+
 }
--- src/main/java/com/example/receitas/services/IngredienteService.java
+++ src/main/java/com/example/receitas/services/IngredienteService.java
@@ -1,12 +1,19 @@
 package com.example.receitas.services;
 
+import com.example.receitas.models.Ingrediente;
 import com.example.receitas.repositorys.IngredienteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class IngredienteService {
 
     IngredienteRepository ingredienteRepository;
+
+    public List<Ingrediente> buscartodos() {
+        return ingredienteRepository.findAll();
+    }
 }
