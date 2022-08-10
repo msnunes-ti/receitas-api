98009117d13e8135b92a308b871b56eaf06b1579
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-08-10 00:30:34 -0300

    Pronto

--- src/main/java/com/example/receitas/dtos/AtualizaIngredienteDTO.java
+++ src/main/java/com/example/receitas/dtos/AtualizaIngredienteDTO.java
@@ -17,5 +17,4 @@ public class AtualizaIngredienteDTO {
 
     @NotNull
     private String quantidadeIngrediente;
-
 }
--- src/main/java/com/example/receitas/mappers/ReceitaMapper.java
+++ src/main/java/com/example/receitas/mappers/ReceitaMapper.java
@@ -48,5 +48,4 @@ public class ReceitaMapper {
         }
         return receitaDTOList;
     }
-
 }
