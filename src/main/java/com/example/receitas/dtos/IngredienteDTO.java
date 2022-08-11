004563e5500fa08ad6e047a57a7279bb60aecac8
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-08-11 19:27:43 -0300

    Pronto

--- src/main/java/com/example/receitas/dtos/IngredienteDTO.java
+++ src/main/java/com/example/receitas/dtos/IngredienteDTO.java
@@ -3,13 +3,18 @@ package com.example.receitas.dtos;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class IngredienteDTO {
 
+    @NotNull
     private Long id;
 
+    @NotNull
     private String nomeIngrediente;
 
+    @NotNull
     private String quantidadeIngrediente;
 }
